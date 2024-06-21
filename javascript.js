78ac1a161074e52f9cd290ae644c2d05f8df4173
nethrabalan-004 <142439779+nethrabalan-004@users.noreply.github.com>
2024-06-21 16:47:44 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+var i1=document.getElementById("i1");
+var i2=document.getElementById("i2");
+var i3=document.getElementById("i3");
+var i4=document.getElementById("i4");
+var i5=document.getElementById("i5");
+var i6=document.getElementById("i6");
+var cha=document.getElementById("cha");
+function ch2()
+{
+    cha.setAttribute("class","bb2");
+    console.log("hi");
+}
+
+function ch3()
+{
+    cha.setAttribute("class","bb3");
+}
+
+function ch4()
+{
+    cha.setAttribute("class","bb4");
+}
+
+function ch5()
+{
+    cha.setAttribute("class","bb5");
+}
+
+function ch6()
+{
+    cha.setAttribute("class","bb6");
+}
+
+function ch1()
+{
+    cha.setAttribute("class","bb1");
+
+}
